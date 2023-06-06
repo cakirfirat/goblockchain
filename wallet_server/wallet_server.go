c58eaabc48f8cea52b277f02800bee1325c1f225
cakirfirat <firatcakir47@gmail.com>
2023-06-07 02:09:24 +0300

    first commit

---
+++ wallet_server/wallet_server.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"blockchain/wallet"
+	"html/template"
+	"io"
+	"log"
+	"net/http"
+	"path"
+	"strconv"
+)
+
+const tempDir = "templates"
+
+type WalletServer struct {
+	port     uint16
+	gatewawy string
+}
+
+func NewWalletServer(port uint16, gateway string) *WalletServer {
+	return &WalletServer{port, gateway}
+}
+func (ws *WalletServer) Port() uint16 {
+	return ws.port
+}
+
+func (ws *WalletServer) Gateway() string {
+	return ws.gatewawy
+}
+
+func (ws *WalletServer) Index(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodGet:
+		t, err := template.ParseFiles(path.Join(tempDir, "index.html"))
+		if err != nil {
+			log.Printf("ERROR: Template parsing failed - %s", err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+		err = t.Execute(w, nil)
+		if err != nil {
+			log.Printf("ERROR: Template execution failed - %s", err.Error())
+			http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+			return
+		}
+	default:
+		log.Printf("ERROR: Invalid HTTP Method")
+	}
+}
+
+func (ws *WalletServer) Wallet(w http.ResponseWriter, req *http.Request) {
+	switch req.Method {
+	case http.MethodPost:
+		w.Header().Add("Content-Type", "application/json")
+		myWallet := wallet.NewWallet()
+		m, _ := myWallet.MarshalJSON()
+		io.WriteString(w, string(m[:]))
+	default:
+		w.WriteHeader(http.StatusBadRequest)
+		log.Println("ERROR: Invalid http method")
+	}
+}
+
+func (ws *WalletServer) Run() {
+	http.HandleFunc("/", ws.Index)
+	http.HandleFunc("/wallet", ws.Wallet)
+	log.Fatal(http.ListenAndServe("0.0.0.0:"+strconv.Itoa(int(ws.Port())), nil))
+}
